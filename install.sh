28e4beb1aa70e3ac32f9622f5a6bf791c2226110
dizkneelande <105323825+dizkneelande@users.noreply.github.com>
2023-04-05 01:33:44 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 ## -- Install packages ---- ##
 read -n1 -rep 'install desktop? it gonna take a while... (y,n)' INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
-	yay -S --noconfirm hyprland-bin hyprpicker ttf-ubuntu-font-family ttf-ubuntu-nerd thunar thunar-archive-plugin xfce4-terminal swaybg swaylock-effects wofi wlogout mako polkit-gnome python-requests swappy grim slurp pamixer brightnessctl gvfs bluez bluez-utils xfce4-settings xdg-desktop-portal-hyprland-git sddm-git noise-suppression-for-voice wl-clipboard wf-recorder noto-fonts-cjk brave-bin bibata-cursor-theme-bin catppuccin-gtk-theme-mocha papirus-icon-theme waybar-hyprland nwg-look kitty noto-fonts-emoji sddm-config-editor-git plymouth-git sddm-catppuccin-git qt5-wayland geany pavucontrol sddm-git pipewire pipewire-{alsa,jack,pulse}
+	yay -S --noconfirm hyprland-git hyprpicker ttf-ubuntu-font-family ttf-ubuntu-nerd thunar thunar-archive-plugin xfce4-terminal swaybg swaylock-effects wofi wlogout mako polkit-gnome python-requests swappy grim slurp pamixer brightnessctl gvfs bluez bluez-utils xfce4-settings xdg-desktop-portal-hyprland-git sddm-git noise-suppression-for-voice wl-clipboard wf-recorder noto-fonts-cjk brave-bin bibata-cursor-theme-bin catppuccin-gtk-theme-mocha papirus-icon-theme waybar-hyprland nwg-look kitty noto-fonts-emoji sddm-config-editor-git plymouth-git sddm-catppuccin-git qt5-wayland geany pavucontrol sddm-git pipewire pipewire-{alsa,jack,pulse}
 	
 	# enable sddm
 	echo -e "enabling sddm service...\n"
